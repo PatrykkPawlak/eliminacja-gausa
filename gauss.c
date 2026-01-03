bea774b08d71b0d72fc79a5df52c9230ab92856e
krzysztof-deb <01206758@pw.edu.pl>
2026-01-03 15:23:03 +0100

    Zmiana wierszy

--- gauss.c
+++ gauss.c
@@ -10,6 +10,12 @@ int eliminate(Matrix *mat, Matrix *b) {
   for (int ir = 0; ir < mat->r; ir++) {
     if (mat->data[ir][0] != 0) {
       czy_osobliwa = 1;
+      double *buf = mat->data[ir];
+      mat->data[ir] = mat->data[0];
+      mat->data[0] = buf;
+      buf = b->data[ir];
+      b->data[ir] = b->data[0];
+      b->data[0] = buf;
       break;
     }
   }
