dc2d0f85f074c08ed225dc2544078b7a77e08c03
PatrykPawlak <01206808@pw.edu.pl>
2026-01-03 14:54:11 +0100

    Napisana implementacja podstawienia wstecznego

--- backsubst.c
+++ backsubst.c
@@ -5,17 +5,19 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
+	if(mat->r!=mat->c)
+		return 2;
+	if(mat->r!=b->r)
+		return 2;
+	for(int i=mat->r-1;i>=0;i--){
+		if(mat->data[i][i]==0)
+			return 1;
+		double sum=0.0;
+		for(int j=i+1;j<mat->c;j++){
+			sum+=mat->data[i][j]*x->data[j][0];
+		}
+		x->data[i][0]=(b->data[i][0]-sum)/mat->data[i][i];
+	}
 				return 0;
 }
 
